f2be0a5ae42ff54a8549e2d1d1666af76f0bbd95
bokage <37666121+bokage@users.noreply.github.com>
2019-05-06 00:07:12 -0700

    Add files via upload
    Added favorite and retweet functionalities.

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,13 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Derek Ye on 4/29/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UICollectionViewCell {
+    
+}
